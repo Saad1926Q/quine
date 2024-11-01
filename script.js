ffdda8297c0633d9adbd3e08854076815ee7d2ce
Saad1926Q <saad.ahmed1926q@gmail.com>
2024-11-01 16:49:04 +0530

    found a viable approach(probably)

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-let y=2;
-let x=1;
-if(x>y){
-    console.log("test")
-}else{
-    console.log("More test")
-}
+// let y=2;
+// let x=1;
+// if(x>y){
+//     console.log("test")
+// }else{
+//     console.log("More test")
+// }
 
-let first;
-let second;
+// let first;
+// let second;
 
-first=`let y=2;
-let x=1;
-if(x>y){
-    console.log("test")
-}else{
-    console.log("More test")
-}
+// first=`let y=2;
+// let x=1;
+// if(x>y){
+//     console.log("test")
+// }else{
+//     console.log("More test")
+// }
 
-let first;
-let second;`
+// let first;
+// let second;`
 
-second="document.getElementById('mydiv').innerHTML=`${first} first=${first} second=${second} ${second}`"
+// second="document.getElementById('mydiv').innerHTML=`${first} first=\`${first}\` second=\"${second}\" ${second}`"
 
-//To include `` inside the template strings we have to escpe using \
+// //To include `` inside the template strings we have to escape using \
 
-document.getElementById('mydiv').innerHTML=`${first} first=\`${first}\` second="${second}" ${second}`
+// document.getElementById('mycontent').innerHTML=`${first}\n\nfirst=\`${first}\`\n\nsecond=\"${second}\"\n\n\n${second}`
+
+(function quine(){
+    let src=`(${quine})()`
+
+    let hello=1092;
+    
+    document.getElementById('mydiv').innerHTML=src;
+    
+})()
